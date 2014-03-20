3bed525ba872b95fcc7a403379ee847eb066d5d2
sravan <sravanreddy29@gmail.com>
2014-03-19 22:05:47 -0400

    Initial Commit

---
+++ CoreDataEncryption/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CoreDataEncryption
+//
+//  Created by SRAVAN on 3/19/14.
+//  Copyright (c) 2014 SRAVAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ CoreDataEncryption/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreDataEncryption
+//
+//  Created by SRAVAN on 3/19/14.
+//  Copyright (c) 2014 SRAVAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
